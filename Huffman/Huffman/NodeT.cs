a25f381ff089f3f5a7a2eb74dc37d01a47f97b6b
33ndX <milanowskijgdiff@gmail.com>
2025-01-08 05:43:12 +0100

    Update 08.01

---
+++ Huffman/Huffman/NodeT.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman
+{
+    internal class NodeT
+    {
+        public NodeT left;
+        public NodeT right;
+        public NodeT parent;
+        public int data;
+
+        public NodeT(int data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+            parent = null;
+        }
+
+        public int GetLiczbaDzieci()
+        {
+            int result = 0;
+            if(this.left != null)
+            {
+                result++;
+            }
+            if(this.right != null)
+            {
+                result++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Huffman/Huffman/NodeTS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman
+{
+    internal class NodeTS : NodeT
+    {
+        public char symbol;
+        public string kod;
+
+        public NodeTS() : base(0) 
+        { 
+            this.symbol = '\0';
+            this.kod = string.Empty;
+        }
+        public NodeTS(char symbol, int freq) : base(freq) 
+        {
+            this.symbol=symbol;
+            this.kod = string.Empty;
+        }   
+    }
+}
